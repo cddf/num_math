91045baee9c19ac257f6c1d0ea620becbb0de725
Christian <sdf@lavabit.com>
2013-10-20 15:24:17 +0200

    Plotten funktioniert halbwegs, aber noch nicht sehr elegant

--- blatt01/5/2.R
+++ blatt01/5/2.R
@@ -1,7 +1,7 @@
 #!/usr/bin/env Rscript
 
 # args: x input-file
-# out:  result-file
+# out:  result-file, plot
 
 args = commandArgs(TRUE)
 
@@ -12,6 +12,12 @@ if(file.access(args[2]) == -1 || file.access(args[2],mode = 4) == -1)
   quit();
 }
 input = read.table(args[2], header=T)
+
+# function to plot
+f = function(x)
+{
+  return( (x-1)^2 )
+}
 
 # (ii)
 wert = function(x, xi, cn)
@@ -28,4 +34,18 @@ wert = function(x, xi, cn)
   }
   return(result)
 }
-write.table (wert(x,input$xi,input$cn), file="2.out", row.names = F, col.names = F)
+pn = wert(x,input$xi,input$cn)
+write.table (pn, file="2.out", row.names = F, col.names = F)
+
+y = c()
+fi = c()
+for(i in -10:10)
+{
+  y = append(y, wert(i,input$xi,input$cn))
+  fi = append(fi,f(i))
+}
+  
+plot(-10:10, y, type="l")
+points(x,pn)
+points(x,f(x))
+lines(-10:10, fi)
